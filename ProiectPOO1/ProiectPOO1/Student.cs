e6b457ca6748a51358e63fc210469d43dcb51597
TSergiu8 <sergiu.tineja@student.upt.ro>
2025-01-15 03:21:42 +0200

    am creat clasa Student

---
+++ ProiectPOO1/ProiectPOO1/Student.cs
@@ -0,0 +1,23 @@
+namespace ProiectPOO1;
+
+public class Student
+{
+    public string Nume { get; set; }
+        public int An { get; set; }
+        public List<Disciplina> Discipline { get; set; }
+        public List<Contestatie> Contestatii { get; set; }
+
+        public Student(string nume, int an)
+        {
+            Nume = nume;
+            An = an;
+            Discipline = new List<Disciplina>();
+            Contestatii = new List<Contestatie>();
+        }
+
+        public void InroleazaDisciplina(Disciplina disciplina)
+        {
+            Discipline.Add(disciplina);
+        }
+        
+}
